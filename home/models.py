434cb43d0f154aadda276b9e076aa2454641d8c6
Overblessing <obmsuya@yahoo.com>
2019-03-21 22:15:15 +0300

    home model

--- home/models.py
+++ home/models.py
@@ -43,23 +43,14 @@ class Item(models.Model):
     name = models.CharField(max_length= 50)
     subtitle = models.CharField (max_length=120, default='', blank=True)
     description = models.TextField()
-    image = models.ImageField(null=True, blank=True,
-            width_field="width_field",
-            height_field="height_field")
-    height_field = models.IntegerField(default=0)
-    width_field = models.IntegerField(default=0)
-    image_author = models.ImageField(null=True, blank=True,
-            width_field="width_field",
-            height_field="height_field")
-    height_field = models.IntegerField(default=0)
-    width_field = models.IntegerField(default=0)
-
-    image_index = models.ImageField(null=True, blank=True,
-            width_field="width_field",
-            height_field="height_field")
-    height_field = models.IntegerField(default=0)
-    width_field = models.IntegerField(default=0)
+    image = models.ImageField(default='default.jpg', upload_to='media')
     
+    image_author = models.ImageField(default='default.jpg', upload_to='media')
+    
+
+    image_index = models.ImageField(default='default.jpg', upload_to='media')
+    
+
     author = models.CharField(max_length=200, default= "")
     author_description = models.TextField(default= "")
     price = models.IntegerField(default=0)
